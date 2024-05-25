5ea2fc64dfc721df4900bbd52fb7291fd38c587f
Piotr Danecki <piotr.a.danecki@gmail.com>
2024-05-25 22:47:22 +0200

    very gud
    Signed-off-by Piotr Danecki <piotr.a.danecki@gmail.com>

--- main.py
+++ main.py
@@ -1,47 +1,32 @@
 #!/bin/python
 from random import randint
-import tty
-import termios
-import sys
-
-class tile:
-    def __init__(self, char):
-        self.char=char
+from pygame import Color
+import pygame
+import time
+class Tile:
+    def __init__(self, color):
+        self.color=color
 
 class Chunk:
-    tiles: list[list[tile]] = []
+    tiles: list[list[Tile]] = []
 
-    def __init__(self, tile_chars: list[str], MAXTILE: int, CHUNKSIZE: int):
-        for i in range(CHUNKSIZE):
-            row: list[int] = []
-            for j in range(CHUNKSIZE):
-                row.append(tile_chars[randint(0, MAXTILE)]);
-            self.tiles.append(row)
+    def __init__(self, CHUNKSIZE: int):
+        self.tiles = [[Tile((randint(0, 255), randint(0, 255), randint(0, 255))) for _ in range(CHUNKSIZE)] for _ in range(CHUNKSIZE)]
+
 
 class World:
-    chunks: list[list[Chunk]] = []
+    chunks = {}
 
-    def __init__(self, tile_chars: list[str], world_size: int, chunk_size: int):
+    def __init__(self, world_size: int, chunk_size: int):
         self.CHUNKSIZE: int = chunk_size
-        self.MAXTILE: int = len(tile_chars)-1
-
-        self.tile_chars: list[str] = tile_chars
         self.world_size: int = world_size
-
-        for i in range(self.world_size):
-            row: list[Chunk] = []
-            for j in range(self.world_size):
-                row.append(Chunk(tile_chars=self.tile_chars, MAXTILE=self.MAXTILE, CHUNKSIZE=self.CHUNKSIZE))
-            self.chunks.append(row)
-
-    def draw_chunk(self, x: int, y: int):
-        for i in range(self.CHUNKSIZE):
-            for j in range(self.CHUNKSIZE):
-                print(self.chunks[x][y].tiles[i][j], end="", flush=True)
-            print()
-
+    
+    def generate_chunk(self, x:int, y:int):
+        if (x, y) not in self.chunks:
+            self.chunks[(x, y)] = Chunk(self.CHUNKSIZE)
 
 class Player:
+    going_right=True
     def __init__(self):
         self.x=0
         self.y=0
@@ -50,69 +35,151 @@ class Player:
     def move(self, x:int, y:int, world:World):
         new_x=self.x+x
         new_y=self.y+y
+
         if new_x==world.CHUNKSIZE:
             self.map_x+=1
             self.x=0
+
         elif new_x==-1:
             self.map_x-=1
             self.x=world.CHUNKSIZE-1
+
         elif new_y==world.CHUNKSIZE-1:
             self.map_y+=1
             self.y=0
+
         elif new_y==-1:
             self.map_y-=1
-            self.y=world.CHUNKSIZE-2
+            self.y=world.CHUNKSIZE-1
+
         else:
             self.x=new_x
             self.y=new_y
         
+running = True
+pygame.init()
+pygame.font.init()
+screensize=720
+statusbar=40
+info=360
+
+screen = pygame.display.set_mode((screensize+info, screensize+statusbar))
+pygame.display.set_caption("pime: python")
+clock = pygame.time.Clock()
+player: Player = Player()
+world: World = World(world_size=16, chunk_size=16)
+dt=1
+
+playerr=pygame.image.load("player/playerr.png")
+playerl=pygame.image.load("player/playerl.png")
+
+sneak_texture=pygame.image.load("gui/sneak_icon.png")
+run_texture=pygame.image.load("gui/run_icon.png")
+
+font=pygame.font.Font("nerdfont.otf",32)
+last_move_time = 0
+status=""
+world.generate_chunk(player.map_x, player.map_y)
+
+# main
+while running:
+    keys=pygame.key.get_pressed()
+   
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
 
 
+    time_period=0.1
+    sneak=False
+    run=False
+    if keys[pygame.K_LSHIFT]:
+        time_period=0.2
+        sneak=True
+    elif keys[pygame.K_LCTRL]:
+        time_period=0.05
+        run=True
+    
+    if keys[pygame.K_ESCAPE]:
+        running=False
 
-def getc() -> str:
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        return sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-
-
-
-def keyboard_handle(char: str, player: Player, world: World) -> None:
-    match char:
-        case 'w':
+    current_time=time.time()
+    if current_time - last_move_time >= time_period:
+        if keys[pygame.K_w]:
             player.move(0, -1, world)
-        case 'a':
-            player.move(-1, 0, world)
-        case 's':
+            last_move_time = current_time
+
+        if keys[pygame.K_s]:
             player.move(0, 1, world)
-        case 'd':
+            last_move_time = current_time
+
+        if keys[pygame.K_a]:
+            player.move(-1, 0, world)
+            last_move_time = current_time
+            player.going_right=False
+
+        if keys[pygame.K_d]:
+            player.going_right=True
+            last_move_time = current_time
             player.move(1, 0, world)
-        case 'q':
-            exit(0)
-        case 'h':
-            print("wasd - move")
-            print("q - quit")
-            print("e - draw")
+   
 
+    if keys[pygame.K_h]:
+        print("wasd - move")
+        print("q - quit")
+   
 
-def main() -> int:
-    player: Player = Player()
-    world: World = World(tile_chars=[",","."], world_size=16, chunk_size=32)
-    running = True
-    while running:
-        print("\n"*50)
-        print("\033[0;0;H", end="")
-        world.draw_chunk(player.map_x, player.map_y);
-        print(f"\033[{player.y+2};{player.x+1}H", end='')
-        print("@", end='')
-        print("\033[100;100;H", end="")
-        print()
-        input_char: str = getc()
-        keyboard_handle(input_char, player, world)
-    return 0
+    screen.fill("black")
 
-if __name__ == '__main__':
-    exit(main())
+    # draw
+    world.generate_chunk(player.map_x, player.map_y)
+    chunk = world.chunks.get((player.map_x,player.map_y))
+
+    tile_size = screensize / world.CHUNKSIZE
+
+    for i in range(world.CHUNKSIZE):
+        for j in range(world.CHUNKSIZE):
+            color = chunk.tiles[i][j].color
+            tileplace_x = i * tile_size
+            tileplace_y = j * tile_size
+            rect = pygame.Rect(tileplace_x, tileplace_y, tile_size, tile_size)
+            pygame.draw.rect(screen, color, rect)
+
+    if player.going_right:
+        playerr_scaled = pygame.transform.scale(playerr, (tile_size, tile_size))
+        screen.blit(playerr_scaled, (player.x*tile_size, player.y*tile_size))
+    else:
+        playerl_scaled = pygame.transform.scale(playerl, (tile_size, tile_size))
+        screen.blit(playerl_scaled, (player.x*tile_size, player.y*tile_size))
+    
+    if run:
+        run_texture_scaled = pygame.transform.scale(run_texture, (tile_size*1.5, tile_size*1.5))
+        screen.blit(run_texture_scaled, (screensize-tile_size*1.5, 0))
+    elif sneak:
+        sneak_texture_scaled = pygame.transform.scale(sneak_texture, (tile_size*1.5, tile_size*1.5))
+        screen.blit(sneak_texture_scaled, (screensize-tile_size*1.5, 0))
+
+    status_text = font.render(status, True, Color(255,255,255,255))
+    status_text_rect = (0,screensize)
+    screen.blit(status_text,status_text_rect)
+   
+    line = 0
+    text = font.render(f"x, y = {player.x}, {player.y}", True, Color(255,255,255,255))
+    text_rect = (screensize, 40*line)
+    screen.blit(text,text_rect)
+    line += 1
+    text = font.render(f"map x, y = {player.map_x}, {player.map_y}", True, Color(255,255,255,255))
+    text_rect = (screensize, 40*line)
+    screen.blit(text,text_rect)
+    line += 1
+    text = font.render(f"over x, y = {player.map_x*world.CHUNKSIZE+player.x}, {player.map_y*world.CHUNKSIZE+player.y}", True, Color(255,255,255,255))
+    text_rect = (screensize, 40*line)
+    screen.blit(text,text_rect)
+    
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000
+
+pygame.font.quit()
+pygame.quit()
+
