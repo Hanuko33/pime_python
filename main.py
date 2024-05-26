54ec9c6ca8ece3bae1b432d6e98e51e789c59ee1
Piotr Danecki <piotr.a.danecki@gmail.com>
2024-05-26 12:56:58 +0200

    bugfix + texture change + food + item
    this commit should be 4
    
    Signed-off-by Piotr Danecki <piotr.a.danecki@gmail.com>

--- main.py
+++ main.py
@@ -6,26 +6,50 @@ import pygame
 import time
 from enum import Enum
 
+solid_path = "textures/items/solid/"
+gas_path = "textures/items/gas/"
+liquid_path = "textures/items/liquid/"
+edible_path = "textures/items/food/"
+textures_solid = []
+textures_solid.append(pygame.image.load(solid_path+"log.png"))
+textures_solid.append(pygame.image.load(solid_path+"sand.png"))
+textures_solid.append(pygame.image.load(solid_path+"stick.png"))
+textures_solid.append(pygame.image.load(solid_path+"stone.png"))
+textures_gas = []
+textures_gas.append(pygame.image.load(gas_path+"gas.png"))
+textures_liquid = []
+textures_liquid.append(pygame.image.load(liquid_path+"water.png"))
+textures_edible = []
+textures_edible.append(pygame.image.load(edible_path+"cherry.png"))
+textures_edible.append(pygame.image.load(edible_path+"pumpkin.png"))
+textures_edible.append(pygame.image.load(edible_path+"watermelon.png"))
+
+BASE_ELEMENTS=100
+
 class forms(Enum):
     SOLID=(0, "solid")
     LIQUID=(1, "liquid")
     GAS=(2, "gas")
-
+    def __init__(self, code, description):
+        self.code = code
+        self.description = description
 class Tile:
     def __init__(self, color):
         self.color=color
 
 class Edible:
     def __init__(self):
-        self.calories = randint(0, 100)
-        self.irrigation = randint(0, 100)
+        self.calories = randint(10, 500)
+        self.irrigation = randint(10, 500)
+        if (randint(0,100)<40):
+            self.poison = randint(200, 1000)
 
 class BaseElement:
     def name_gen(self) -> str:
         vowels = "aeiou"
         consonants = "bcdfghjklmnpqrstvwxyz"
         
-        length = randint(2, 10)        
+        length = randint(2, 6)        
 
         word = []
         for i in range(length):
@@ -39,23 +63,30 @@ class BaseElement:
     def __init__(self):
         self.form = random.choice(list(forms))
         self.name = self.name_gen()
-        if (randint(0, 100) < 40):
+        if self.form == forms.SOLID:
+            self.texture = random.choice(textures_solid)
+        if self.form == forms.GAS:
+            self.texture = random.choice(textures_gas)
+        if self.form == forms.LIQUID:
+            self.texture = random.choice(textures_liquid)
+        if (randint(0, 100) < 25):
             self.edible = Edible()
+            self.texture = random.choice(textures_edible)
+
 base_elements: list[BaseElement] = []
-for i in range(50):
+for i in range(BASE_ELEMENTS):
     base_elements.append(BaseElement())
 
 class Item:
     def __init__(self, x:int, y:int, in_inventory:bool):
-        self.base_element = base_elements[randint(0,49)]
+        self.base_element = base_elements[randint(0,BASE_ELEMENTS-1)]
         self.x=x
         self.y=y
         self.in_inventory=in_inventory
-        self.texture=pygame.image.load("items/item.png")
     def show(self):
         base_element = self.base_element.name
         print(f'{base_element=}')
-        form = self.base_element.form.name
+        form = self.base_element.form.description
         print(f'{form=}')
         if hasattr(item.base_element, 'edible'):
             print("*** Edible ***")
@@ -63,6 +94,10 @@ class Item:
             irrigation = self.base_element.edible.irrigation
             print(f'    {calories=}')
             print(f'    {irrigation=}')
+            if hasattr(item.base_element.edible, "poison"):
+                print("    *** Poison ***")
+                poison = self.base_element.edible.poison
+                print(f'        {poison=}')
 
 class Chunk:
     tiles: list[list[Tile]] = []
@@ -86,6 +121,9 @@ class World:
 
 class Player:
     inventory: list[Item] = []
+    selected_inventory: int = 0
+    irrigation: int = 500
+    saturation: int = 500
     going_right=True
     def __init__(self):
         self.x=0
@@ -93,6 +131,8 @@ class Player:
         self.map_x=0
         self.map_y=0
     def move(self, x:int, y:int, world:World):
+        self.irrigation-=3
+        self.saturation-=3
         new_x=self.x+x
         new_y=self.y+y
 
@@ -121,7 +161,7 @@ pygame.init()
 pygame.font.init()
 screensize=720
 statusbar=40
-info=360
+info=400
 
 screen = pygame.display.set_mode((screensize+info, screensize+statusbar))
 pygame.display.set_caption("pime: python")
@@ -130,11 +170,17 @@ player: Player = Player()
 world: World = World(world_size=16, chunk_size=16)
 dt=1
 
-playerr=pygame.image.load("player/playerr.png")
-playerl=pygame.image.load("player/playerl.png")
+def get_item_at_ppos() -> Item:
+    for item in chunk.items:
+        if (item.x == player.x) and (item.y == player.y):
+            return item
+    return None
 
-sneak_texture=pygame.image.load("gui/sneak_icon.png")
-run_texture=pygame.image.load("gui/run_icon.png")
+playerr=pygame.image.load("textures/player/playerr.png")
+playerl=pygame.image.load("textures/player/playerl.png")
+
+sneak_texture=pygame.image.load("textures/gui/sneak_icon.png")
+run_texture=pygame.image.load("textures/gui/run_icon.png")
 
 font=pygame.font.Font("nerdfont.otf",32)
 last_move_time = 0
@@ -152,16 +198,44 @@ while running:
         if event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_SEMICOLON:
-                for item in chunk.items:
-                    if (item.x == player.x) and (item.y == player.y):
-                        print("\n\n\n")
-                        item.show()
-    
-            if event.key == pygame.K_e:
+                if len(player.inventory):
+                    item = player.inventory[player.selected_inventory]
+                    item.show()
+            if event.key == pygame.K_f:
+                if len(player.inventory) and hasattr(player.inventory[player.selected_inventory].base_element, "edible"):
+                    edible=player.inventory[player.selected_inventory].base_element.edible
+                    player.saturation+=edible.calories
+                    player.irrigation+=edible.irrigation
+
+                    if hasattr(edible, "poison"):
+                        player.saturation-=edible.poison
+                        player.irrigation-=edible.poison
+
+                    if player.selected_inventory > 0:
+                        player.selected_inventory-=1
+                    player.inventory.remove(player.inventory[player.selected_inventory])
+            if event.key == pygame.K_q:
+                player.inventory[player.selected_inventory].x = player.x
+                player.inventory[player.selected_inventory].y = player.y
+                world.chunks.get((player.map_x, player.map_y)).items.append(player.inventory[player.selected_inventory])
+                player.inventory.remove(player.inventory[player.selected_inventory])
+            if event.key == pygame.K_TAB: 
+                if player.selected_inventory<(len(player.inventory)-1):
+                    player.selected_inventory+=1
+                else:
+                    player.selected_inventory=0
+            if event.key == pygame.K_BACKQUOTE:
+                if player.selected_inventory>0:
+                    player.selected_inventory-=1 
+                else:
+                    player.selected_inventory=len(player.inventory)-1
+
+            if event.key == pygame.K_e or event.key == pygame.K_RETURN:
                 for item in chunk.items:
                     if (item.x == player.x) and (item.y == player.y):
                         player.inventory.append(item)
                         world.chunks.get((player.map_x, player.map_y)).items.remove(item)
+                        break
             if event.key == pygame.K_i:
                 print("\n\n\n")
                 for item in player.inventory:
@@ -224,7 +298,7 @@ while running:
             pygame.draw.rect(screen, color, rect)
 
     for item in chunk.items:
-        texture = item.texture
+        texture = item.base_element.texture
         x = item.x * tile_size
         y = item.y * tile_size
         texture_scaled = pygame.transform.scale(texture, (tile_size, tile_size))
@@ -260,6 +334,40 @@ while running:
     text = font.render(f"over x, y = {player.map_x*world.CHUNKSIZE+player.x}, {player.map_y*world.CHUNKSIZE+player.y}", True, Color(255,255,255,255))
     text_rect = (screensize, 40*line)
     screen.blit(text,text_rect)
+
+    line += 1
+    text = font.render(f"saturation = {player.saturation}", True, Color(255,255,255,255))
+    text_rect = (screensize, 40*line)
+    screen.blit(text,text_rect)
+    
+    line += 1
+    text = font.render(f"irrigation = {player.irrigation}", True, Color(255,255,255,255))
+    text_rect = (screensize, 40*line)
+    screen.blit(text,text_rect)
+
+    line += 1
+    if get_item_at_ppos():
+        text = font.render(f"item: {get_item_at_ppos().base_element.form.description} ({get_item_at_ppos().base_element.name})", True, Color(255,255,255,255))
+        text_rect = (screensize, 40*line)
+        screen.blit(text,text_rect)
+            
+    for i in range(len(player.inventory)):
+        if ((i%8) == 0) and i > 0:
+            line +=1
+
+        if i!=player.selected_inventory:
+            color = Color(200,200,200,255)
+        else:
+            color = Color(0, 200, 0, 255)
+
+        x = screensize+tile_size*(i%8)
+        y = line*50
+        rect = pygame.Rect(x, y, tile_size, tile_size)
+        pygame.draw.rect(screen, color, rect, 2)
+        texture = player.inventory[i].base_element.texture
+        texture_scaled = pygame.transform.scale(texture, (tile_size, tile_size))
+        screen.blit(texture_scaled, (x, y))       
+
     
     pygame.display.flip()
     dt = clock.tick(60) / 1000
